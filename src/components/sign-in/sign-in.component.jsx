aa8dda8d77cd7ec55b85b106b790b8017d6b5fda
rui li <rli925@usc.edu>
2019-07-29 15:41:06 -0700

    sign up with email and password

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
-import CustomButton from '../custom-buttom/custom-buttom.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import { signInWithGoogle } from '../../firebase/firebase.utils';
 
