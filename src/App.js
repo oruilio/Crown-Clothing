ae364c44f7d2621961923993fca43c72267a6925
rui li <rli925@usc.edu>
2019-07-29 12:19:10 -0700

    firebase: storing user data in firebase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor(){
@@ -21,8 +21,8 @@ class App extends React.Component {
   unsubsribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubsribeFromAuth = auth.onAuthStateChanged( user => {
-      this.setState({ currentUser: user })
+    this.unsubsribeFromAuth = auth.onAuthStateChanged( async user => {
+      createUserProfileDocument(user);
     })
   }
 
