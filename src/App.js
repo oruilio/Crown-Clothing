8f32c2d6277b59deaaef2657a8f3c45a9c3501a8
rui li <rli925@usc.edu>
2019-07-31 16:00:32 -0700

    user redirect & user action type

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
@@ -46,7 +46,12 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignOutPage} />
+          <Route exact path='/signin' render={ 
+            () => this.props.currentUser ? ( 
+            <Redirect to='/' /> 
+            ) : (
+            <SignInAndSignOutPage /> ) } 
+          />
         </Switch>
       </div>
     );
@@ -54,8 +59,11 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
