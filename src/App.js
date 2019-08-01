27f4c0c9cca461b9fc37a83f04d46d9337c7d2dc
rui li <rli925@usc.edu>
2019-08-01 16:57:25 -0700

    Checkout Page & Checkout Item Component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser} from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
@@ -49,6 +51,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={ 
             () => this.props.currentUser ? ( 
             <Redirect to='/' /> 
