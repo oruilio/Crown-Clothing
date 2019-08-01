dfbecad3ff57c5c65528ad025b16ffeef7c939d6
rui li <rli925@usc.edu>
2019-07-31 22:25:57 -0700

    Add to Cart styling

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import CustomButton from '../custom-button/custom-button.component';
+
 import './collection-item.styles.scss';
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
@@ -12,7 +14,8 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
         <div className='collection-footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
-        </div>       
+        </div>   
+        <CustomButton inverted>Add to Cart</CustomButton>    
     </div>
 )
 
