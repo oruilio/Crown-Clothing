eee12dfcd4226aedf2643306c7b7a64aff13cba1
rui li <rli925@usc.edu>
2019-08-01 17:17:54 -0700

    Remove Items From Cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -14,10 +14,15 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         hidden: !state.hidden
       };
     case CartActionTypes.ADD_ITEM:
-        return {
-            ...state,
-            cartItems: addItemToCart(state.cartItems, action.payload)
+      return {
+          ...state,
+          cartItems: addItemToCart(state.cartItems, action.payload)
         };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter( cartItem => cartItem.id !== action.payload.id ) 
+      }
     default:
       return state;
   }
