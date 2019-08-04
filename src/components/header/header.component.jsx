70548f8184e41734d7f6eae0987c8e6c38eba4e4
rui li <rli925@usc.edu>
2019-08-04 12:56:29 -0700

    [css->css in JSX] improvement of AS

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -10,7 +10,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
 
-import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink, OptionDiv} from './header.styles';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from './header.styles';
 
 const Header = ( { currentUser, hidden }) => (
     <HeaderContainer>
@@ -27,9 +27,9 @@ const Header = ( { currentUser, hidden }) => (
             {
                 currentUser ?
                 (
-                    <OptionDiv className='option' onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+                    <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
                 ):(
-                    <OptionLink className='option' to='/signin'>SIGN IN</OptionLink>
+                    <OptionLink to='/signin'>SIGN IN</OptionLink>
                 )
             }
             <CartIcon />
