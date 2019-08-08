f3c195843d05100492b6c5d9232c1ab2a6d5b7ba
rui li <rli925@usc.edu>
2019-08-08 12:21:52 -0700

    [Redux-Saga] Google Sign In Into Sagas

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import {connect} from 'react-redux';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth } from '../../firebase/firebase.utils';
+import { googleSignInStart } from '../../redux/user/user.actions';
 
 import './sign-in.styles.scss';
 
@@ -37,6 +39,8 @@ class SignIn extends React.Component{
     }
 
     render(){
+        const { googleSignInStart } = this.props;
+
         return(
             <div className ='sign-in'>
                 <h2>I already have an account</h2>
@@ -59,8 +63,10 @@ class SignIn extends React.Component{
                         required 
                     />
                     <div className='buttons'>
-                        <CustomButton type="submit"> Sign in </CustomButton>
-                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google </CustomButton>
+                        <CustomButton type='submit'> Sign in </CustomButton>
+                        <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn> 
+                            Sign in with Google 
+                        </CustomButton>
                     </div>
 
                 </form>
@@ -70,4 +76,8 @@ class SignIn extends React.Component{
     }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+    googleSignInStart: () => dispatch(googleSignInStart())
+})
+
+export default connect(null,mapDispatchToProps)(SignIn);
---
+++ src/redux/user/user.sagas.js
@@ -0,0 +1,40 @@
+import { takeLatest, put, all, call } from 'redux-saga/effects';
+
+import UserActionTypes from './user.types';
+
+import {
+  signInSuccess,
+  signInFailure,
+} from './user.actions';
+
+import {
+  auth,
+  googleProvider,
+  createUserProfileDocument,
+} from '../../firebase/firebase.utils';
+
+export function* signInWithGoogle() {
+    try{
+        const {user} = yield auth.signInWithPopup(googleProvider);     //we pull out googleProvider because we wanna access to userRef
+        const userRef = yield call(createUserProfileDocument, user);
+        const userSnapshot = yield userRef.get();
+        yield put(
+            signInSuccess({
+                id:userSnapshot.id, 
+                ...userSnapshot.data()
+            })
+        )
+    }
+    catch(error){
+        yield put(signInFailure(error));
+    }
+}
+    
+
+export function* onGoogleSignInStart() {
+    yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle)
+}
+
+export function* userSagas(){
+    yield all([call(onGoogleSignInStart)])
+}
