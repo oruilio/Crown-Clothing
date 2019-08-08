e7869a9f07eff017694fd33b68a0fb960e9a9ddf
rui li <rli925@usc.edu>
2019-08-08 13:04:55 -0700

    [Redux-Saga] Email Sign In Into Sagas

--- src/App.js
+++ src/App.js
@@ -11,46 +11,9 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import CheckoutPage from './pages/checkout/checkout.component';
 
-//import { auth, createUserProfileDocument/*, addCollectionAndDocuments*/ } from './firebase/firebase.utils';
-import { setCurrentUser} from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
-//import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component {
-  
-  unsubsribeFromAuth = null;
-
- /*
-  componentDidMount(){
-    const {setCurrentUser} = this.props;
-
-    //param userAuth comes from firebase, meaning sb login, onAuthStateChanged is an open function, so we need to close it when the app unmount
-    this.unsubsribeFromAuth = auth.onAuthStateChanged( async userAuth => {
-      if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
-        
-        //to check whether our database has updated at userRef with any new data
-        //keep listening
-        userRef.onSnapshot(snapShot => {
-          setCurrentUser({
-            id: snapShot.id,
-            ...snapShot.data()
-          });
-        });
-      }
-      else{
-        setCurrentUser(userAuth);
-      }
-      //execute only one time:
-      //addCollectionAndDocuments('collections', collectionsArray.map(({title, items}) => ({title, items})));
-    })
-  }
-
-  componentWillUnmount(){
-    this.unsubsribeFromAuth();
-  }
-
-*/
 
   render(){
     return (
@@ -75,11 +38,6 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  //collectionsArray: selectCollectionsForPreview
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
