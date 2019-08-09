92f80550e9f9c489ec8513944448b079764c3d19
rui li <rli925@usc.edu>
2019-08-08 22:59:52 -0700

    [Redux-Saga] Recreating Persistence

--- src/App.js
+++ src/App.js
@@ -12,8 +12,20 @@ import SignInAndSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-u
 import CheckoutPage from './pages/checkout/checkout.component';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { checkUserSession } from './redux/user/user.actions';
 
 class App extends React.Component {
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    const { checkUserSession } = this.props;
+    checkUserSession();
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
 
   render(){
     return (
@@ -40,4 +52,8 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch =>({
+  checkUserSession: () => dispatch(checkUserSession())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -11,6 +11,7 @@ import {
   auth,
   googleProvider,
   createUserProfileDocument,
+  getCurrentUser
 } from '../../firebase/firebase.utils';
 
 export function* getSnapshotFromUserAuth(userAuth) {
@@ -32,7 +33,6 @@ export function* signInWithGoogle() {
     }
 }
     
-
 export function* onGoogleSignInStart() {
     yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -50,6 +50,20 @@ export function* onEmailSignInStart() {
     yield takeLatest(UserActionTypes.EMAIL_SIGN_IN_START, signInWithEmail);
 }
 
+export function* isUserAuthenticated() {
+    try {
+        const userAuth = yield getCurrentUser();
+        if (!userAuth) return;
+        yield getSnapshotFromUserAuth(userAuth);
+    } catch (error) {
+        yield put(signInFailure(error));
+    }
+}
+
+export function* onCheckUserSession() {
+    yield takeLatest(UserActionTypes.CHECK_USER_SESSION,isUserAuthenticated);
+}
+
 export function* userSagas(){
-    yield all([call(onGoogleSignInStart), call(onEmailSignInStart)]);
+    yield all([call(onGoogleSignInStart), call(onEmailSignInStart), call(onCheckUserSession)]);
 }
